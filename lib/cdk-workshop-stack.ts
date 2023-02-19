9fb39876a1719fe3d70fefca1d19b7901e53cbea
crochetcode <fred.davison@outlook.com>
2023-02-19 15:33:41 -0600

    cdk init

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,31 +1,19 @@
-import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigw from 'aws-cdk-lib/aws-apigateway';
-import {HitCounter} from './hitcounter';
-import {TableViewer} from 'cdk-dynamo-table-viewer';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
 
-export class CdkWorkshopStack extends cdk.Stack {
-    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+export class CdkWorkshopStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-        const hello = new lambda.Function(this, 'HelloHandler', {
-            runtime: lambda.Runtime.NODEJS_14_X,
-            code: lambda.Code.fromAsset('lambda'),
-            handler: 'hello.handler'
-        });
-        
-        const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
-           downstream: hello
-        });
+    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
+      visibilityTimeout: Duration.seconds(300)
+    });
 
-        new apigw.LambdaRestApi(this, 'Endpoint', {
-            handler: helloWithCounter.handler
-        });
+    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
 
-        new TableViewer(this, 'ViewHitCounter', {
-            table: helloWithCounter.table,
-            title: 'Hello Hits',
-            sortBy: '-hits'
-        });
-    }
+    topic.addSubscription(new subs.SqsSubscription(queue));
+  }
 }
